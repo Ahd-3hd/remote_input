74624034fa55b7cc7744b6e9e5c6891b9450a524
Ahd Hani <ahdhani0@gmail.com>
2023-12-17 22:53:05 +0300

    fix more stuff

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO, emit
 import socketio
+from flask_cors import CORS
 
 app = Flask(__name__)
-socketio_main = SocketIO(app)
+CORS = CORS(app, resources={r"/*": {"origins": "*"}})
+socketio_main = SocketIO(app,cors_allowed_origins="*")
 # socketio_second = socketio.Client()
 
 # socketio_second.connect('http://127.0.0.1:5001')  # Adjust the URL accordingly
